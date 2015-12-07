ba8c4dd4c1b7b6fd4935cffcc8b3749e1738a516
jenhail <hello@jenniferhail.com>
2015-12-06 21:44:21 -0500

    add css for one-third layout

--- site/snippets/newsletter.php
+++ site/snippets/newsletter.php
@@ -4,11 +4,11 @@
 <form action="//bypine.us10.list-manage.com/subscribe/post?u=90363b999232548e9532004b0&amp;id=874c472f49" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
     <div id="mc_embed_signup_scroll">
 	
-<div class="mc-field-group">
+<div class="mc-field-group one-third">
 	<label for="mce-FNAME">Name </label>
 	<input type="text" value="name" name="FNAME" class="" id="mce-FNAME">
 </div>
-<div class="mc-field-group">
+<div class="mc-field-group one-third">
 	<label for="mce-EMAIL">Email </label>
 	<input type="email" value="email" name="EMAIL" class="required email" id="mce-EMAIL">
 </div>
@@ -17,7 +17,7 @@
 		<div class="response" id="mce-success-response" style="display:none"></div>
 	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_90363b999232548e9532004b0_874c472f49" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Join" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    <div class="clear one-third"><input type="submit" value="Join" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
     </div>
 </form>
 </div>
