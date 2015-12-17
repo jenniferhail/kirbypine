9174beae76b5b24841c4f8022efdfdd61b782c00
jenhail <hello@jenniferhail.com>
2015-12-16 20:03:08 -0500

    fix nav javascript

---
+++ site/blueprints/blog.php
@@ -0,0 +1,13 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Blog
+pages:
+  template: blogpost
+files: false
+fields:
+  title:
+    label: Title
+    type:  text
+  description:
+    label: Page Description
+    type:  text 
--- site/snippets/header.php
+++ site/snippets/header.php
@@ -12,22 +12,19 @@
   <?php echo css('assets/css/style.css') ?>
   <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
   <script type="text/javascript"> 
-     $(document).ready(function() {
-        $('#menubtn').click(function(m) {
-          m.preventDefault();
-          m.stopPropagation();
-          $('#menuopen').fadeToggle();
-          $('header').addClass('full-height');
-          return false;
-         });
-        $('#menuopen').click(function(m) {
-          m.stopPropagation();
-        });
-        // $(document).click(function() {
-        //   $('#menuopen').fadeToggle();
-        //   $('.main').toggleClass('fade');
-        // }); 
-     });
+    $( document ).ready( function () {
+      $('#menubtn').click(function () {
+        if(!$('#menuopen').is(':visible')){
+          $('#menuopen').fadeIn('slow', function() {
+            $('.header').toggleClass('full-height');
+          });
+        } else {
+          $('#menuopen').fadeOut('slow', function() {
+            $('.header').toggleClass('full-height');
+          });
+        }
+      });
+    });   
   </script>
 
 </head>
@@ -41,7 +38,7 @@
         <a href="#" id="menubtn"><span>Menu —</span></a>
       </div>
     </div>
-    <div id="menuopen" class="fade" style="display: none;">
+    <div id="menuopen" style="display: none;">
       <?php snippet('menu') ?>
     </div>
 <!--     <a class="logo left" href="<?php echo url() ?>">
