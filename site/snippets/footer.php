d17bb7cfdc5163abfbf145b66e1bc7f21028bec6
jenhail <hello@jenniferhail.com>
2016-01-04 13:57:22 -0500

    edit footer

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,15 +1,3 @@
-<!--   <footer class="footer cf" role="contentinfo">
-
-    <div class="copyright left">
-      <h1><?php echo $site->footerTitle()->html() ?></h1>
-    </div>
-
-    <div class="colophon right">
-      <?php echo $site->footer()->kirbytext() ?>
-    </div>
-
-  </footer> -->
-
 </div> <!-- End .wrap -->
 
 <script id="dsq-count-scr" src="//pine.disqus.com/count.js" async></script>
