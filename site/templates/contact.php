7d207360308fc7c3e8724e90547ee3ebc85b04d9
Jennifer Hail <jennifer@bypine.co>
2016-08-06 15:37:36 -0700

    update type styles

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -2,13 +2,7 @@
 
   <main class="main" role="main">
 
-    <?php if($image = $page->image('pine-contact.jpg')): ?>
-    <section>
-      <figure class="landscape">
-        <img src="<?php echo $image->url() ?>" alt="<?php echo html($image->title()) ?>">
-      </figure>
-    </section>
-    <?php endif ?>
+
 
     <section>
       <?php echo $page->text()->kirbytext() ?>
