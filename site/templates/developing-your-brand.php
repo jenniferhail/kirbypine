638badb84d8b466bb2432f3c84f2df724d00d09e
Jennifer Hail <jennifer@bypine.co>
2017-05-19 14:09:59 -0400

    add custom class page template

---
+++ site/templates/developing-your-brand.php
@@ -0,0 +1,11 @@
+<?php snippet('header') ?>
+
+	<main class="main" role="main">
+
+		<section class="text">
+			<script type='text/javascript' src='//app.dubsado.com/plugins/iframeResizer.min.js'></script><iframe src='https://hello.dubsado.com:443/public/form/view/591f31ca3d5e33187a886306' frameBorder='0' width='100%' height='750'></iframe><script type='text/javascript'>setTimeout(function(){iFrameResize({checkOrigin: false});},30);</script>
+		</section>
+
+	</main>
+
+<?php snippet('footer') ?>
