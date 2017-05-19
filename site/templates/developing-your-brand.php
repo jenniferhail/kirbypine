c8abf174deb173421fe9f4cce6cd4183c4bd5786
Jennifer Hail <jennifer@bypine.co>
2017-05-19 14:11:53 -0400

    class page edits

--- site/templates/developing-your-brand.php
+++ site/templates/developing-your-brand.php
@@ -3,6 +3,10 @@
 	<main class="main" role="main">
 
 		<section class="text">
+			<?php echo $page->text()->kirbytext() ?>
+		</section>
+
+		<section class="text">
 			<script type='text/javascript' src='//app.dubsado.com/plugins/iframeResizer.min.js'></script><iframe src='https://hello.dubsado.com:443/public/form/view/591f31ca3d5e33187a886306' frameBorder='0' width='100%' height='750'></iframe><script type='text/javascript'>setTimeout(function(){iFrameResize({checkOrigin: false});},30);</script>
 		</section>
 
